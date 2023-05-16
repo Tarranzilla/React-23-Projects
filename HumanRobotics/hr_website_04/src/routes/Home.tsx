0752b80780f1285e7fc1df417b408e2074f33d85
JOAO TARRAN <joaotarran@gmail.com>
2023-05-16 17:44:45 -0300

    robos inicio layout

--- HumanRobotics/hr_website_04/src/routes/Home.tsx
+++ HumanRobotics/hr_website_04/src/routes/Home.tsx
@@ -4,9 +4,35 @@ export default function Home() {
     return (
         <>
             <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="Route_Container">
-                <h1 className="Route_Title">O futuro da robótica é Interativo</h1>
-                <h1 className="Route_Title">O futuro da robótica é Humano</h1>
-                <h1 className="Route_Title">O futuro da robótica é Agora</h1>
+                <div className="Home_Title_Group">
+                    <m.h1
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ delay: 1.1 }}
+                        className="Route_Title Home_Title"
+                    >
+                        O futuro da robótica é <strong>Interativo</strong>
+                    </m.h1>
+                    <m.h1
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ delay: 1.1 }}
+                        className="Route_Title Home_Title"
+                    >
+                        O futuro da robótica é <strong>Humano</strong>
+                    </m.h1>
+                    <m.h1
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ delay: 1.1 }}
+                        className="Route_Title Home_Title"
+                    >
+                        O futuro da robótica é <strong>Agora</strong>
+                    </m.h1>
+                </div>
             </m.div>
         </>
     );
--- HumanRobotics/hr_website_04/src/routes/Robots.tsx
+++ HumanRobotics/hr_website_04/src/routes/Robots.tsx
@@ -1,10 +1,36 @@
 import { motion as m } from "framer-motion";
 
+import Avatar from "../assets/images/RobiOS_AVATAR.png";
+import Go from "../assets/images/RobiOS_GO.png";
+import Inspector from "../assets/images/RobiOS_INSPECTOR.png";
+import Cargo from "../assets/images/RobiOS_CARGO.png";
+
 export default function Robots() {
     return (
         <>
             <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="Route_Container">
                 <h1 className="Route_Title">Robots</h1>
+                <div className="Robots_Container">
+                    <div className="Robot_Card">
+                        <h2>Avatar</h2>
+                        <img src={Avatar} className="Robot_Main_Image"></img>
+                    </div>
+
+                    <div className="Robot_Card">
+                        <h2>GO</h2>
+                        <img src={Go} className="Robot_Main_Image"></img>
+                    </div>
+
+                    <div className="Robot_Card">
+                        <h2>INSPECTOR</h2>
+                        <img src={Inspector} className="Robot_Main_Image"></img>
+                    </div>
+
+                    <div className="Robot_Card">
+                        <h2>CARGO</h2>
+                        <img src={Cargo} className="Robot_Main_Image"></img>
+                    </div>
+                </div>
             </m.div>
         </>
     );
