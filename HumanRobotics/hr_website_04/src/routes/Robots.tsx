4ba529c4699dc27056cc0643c6246d0e557dd85b
JOAO TARRAN <joaotarran@gmail.com>
2023-05-23 18:41:56 -0300

    Robots Update (wip)

--- HumanRobotics/hr_website_04/src/routes/Robots.tsx
+++ HumanRobotics/hr_website_04/src/routes/Robots.tsx
@@ -1,9 +1,13 @@
 import { motion as m } from "framer-motion";
 
-import Avatar from "../assets/images/RobiOS_AVATAR.png";
-import Go from "../assets/images/RobiOS_GO.png";
-import Inspector from "../assets/images/RobiOS_INSPECTOR.png";
-import Cargo from "../assets/images/RobiOS_CARGO.png";
+import Avatar from "../assets/images/Robios_AVATAR_New.png";
+import AvatarBg from "../assets/images/Robios_AVATAR_New_Background.png";
+import Go from "../assets/images/Robios_GO_New.png";
+import GoBg from "../assets/images/Robios_GO_New_Background.png";
+import Inspector from "../assets/images/Robios_INSPECTOR_New.png";
+import InspectorBg from "../assets/images/Robios_INSPECTOR_New_Background.png";
+import Cargo from "../assets/images/Robios_CARGO_New.png";
+import CargoBg from "../assets/images/Robios_CARGO_New_Background.png";
 
 export default function Robots() {
     return (
@@ -11,23 +15,40 @@ export default function Robots() {
             <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="Route_Container">
                 <div className="Robots_Container">
                     <div className="Robot_Card">
-                        <h2>Avatar</h2>
+                        <div className="Robot_Card_Title_Container">
+                            <h3 className="Robot_Card_Title Robot_Card_SubTitle">RobiOS</h3>
+                            <h2 className="Robot_Card_Title">AVATAR</h2>
+                        </div>
                         <img src={Avatar} className="Robot_Main_Image"></img>
+                        <img src={AvatarBg} className="Robot_Background_Image"></img>
                     </div>
 
                     <div className="Robot_Card">
-                        <h2>GO</h2>
+                        <div className="Robot_Card_Title_Container">
+                            <h3 className="Robot_Card_Title Robot_Card_SubTitle">RobiOS</h3>
+                            <h2 className="Robot_Card_Title">GO</h2>
+                        </div>
                         <img src={Go} className="Robot_Main_Image"></img>
+                        <img src={GoBg} className="Robot_Background_Image"></img>
                     </div>
 
                     <div className="Robot_Card">
-                        <h2>INSPECTOR</h2>
+                        <div className="Robot_Card_Title_Container">
+                            <h3 className="Robot_Card_Title Robot_Card_SubTitle">RobiOS</h3>
+                            <h2 className="Robot_Card_Title">INSPECTOR</h2>
+                        </div>
+
                         <img src={Inspector} className="Robot_Main_Image"></img>
+                        <img src={InspectorBg} className="Robot_Background_Image"></img>
                     </div>
 
                     <div className="Robot_Card">
-                        <h2>CARGO</h2>
+                        <div className="Robot_Card_Title_Container">
+                            <h3 className="Robot_Card_Title Robot_Card_SubTitle">RobiOS</h3>
+                            <h2 className="Robot_Card_Title">CARGO</h2>
+                        </div>
                         <img src={Cargo} className="Robot_Main_Image"></img>
+                        <img src={AvatarBg} className="Robot_Background_Image"></img>
                     </div>
                 </div>
             </m.div>
