1c941c1627d3e5d7e92c93aff5e8ff806fb0261a
JOAO TARRAN <joaotarran@gmail.com>
2023-06-26 21:58:23 -0300

    update

--- 02_Projetos/02_TropicalCacau/tc_website_final/src/components/02B_Search_Bar.tsx
+++ 02_Projetos/02_TropicalCacau/tc_website_final/src/components/02B_Search_Bar.tsx
@@ -2,7 +2,7 @@ import { motion as m } from "framer-motion";
 
 export default function SearchBar() {
     return (
-        <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="SearchBar" key={"SearchBar"}>
+        <m.div initial={{ x: -400 }} animate={{ x: 0 }} exit={{ x: -600 }} className="SearchBar" key={"SearchBar"}>
             <p>SearchBar</p>
         </m.div>
     );
--- 02_Projetos/02_TropicalCacau/tc_website_final/src/components/02C_Shopping_Cart.tsx
+++ 02_Projetos/02_TropicalCacau/tc_website_final/src/components/02C_Shopping_Cart.tsx
@@ -2,7 +2,7 @@ import { motion as m } from "framer-motion";
 
 export default function Shopping_Cart() {
     return (
-        <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="Shopping_Cart" key={"Shopping_Cart"}>
+        <m.div initial={{ x: 600 }} animate={{ x: 0 }} exit={{ x: 600 }} className="Shopping_Cart" key={"Shopping_Cart"}>
             <p>Shopping_Cart</p>
         </m.div>
     );
