0e3e426a52f1886af1bb33f79f6c4e1d9eae7ee7
JOAO TARRAN <joaotarran@gmail.com>
2023-06-29 18:24:59 -0300

    Files Upload

--- 02_Projetos/02_TropicalCacau/tc_website_final/src/components/LP_Sections/LP_Section_03_Produtos.tsx
+++ 02_Projetos/02_TropicalCacau/tc_website_final/src/components/LP_Sections/LP_Section_03_Produtos.tsx
@@ -1,9 +1,17 @@
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 
 const LP_Section_03_Produtos = forwardRef(function LP_Section_03_Produtos(props, ref: any) {
+    const [activeType, setActiveType] = useState(null);
+
     return (
         <div className="LP_Section LP_Section_03_Produtos" id="LP_Section_3" ref={ref} key={"LP_Section_3"}>
             <p>Produtos</p>
+            <div className="Product_Types_Container">
+                <div className="Product_Type">Clássicos</div>
+                <div className="Product_Type">Especiais</div>
+                <div className="Product_Type">Combincações</div>
+                <div className="Product_Type">Assinatura</div>
+            </div>
         </div>
     );
 });
