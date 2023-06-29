c4542e5546b52fcfb1621a70fc727900e5439160
JOAO TARRAN <joaotarran@gmail.com>
2023-06-29 17:39:27 -0300

    Menu_Update

--- 02_Projetos/02_TropicalCacau/tc_website_final/src/components/02_Menu.tsx
+++ 02_Projetos/02_TropicalCacau/tc_website_final/src/components/02_Menu.tsx
@@ -1,9 +1,119 @@
+//Framer Motion Imports
 import { motion as m } from "framer-motion";
 
+// Redux Imports
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../context/main_context";
+
 export default function Menu() {
+    const dispatch = useDispatch();
+
+    const toggleMenuButton = () => {
+        dispatch(toggleMenu());
+    };
+
     return (
         <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="Menu" key={"Menu"}>
-            <p>Menu</p>
+            <div className="Menu_Header">
+                <span className="material-icons">home</span>
+                <p className="Menu_Title">Menu</p>
+                <button className="Searchbar_Close_Btn" onClick={toggleMenuButton}>
+                    <span className="material-icons">close</span>
+                </button>
+            </div>
+            <div className="Menu_List_Container">
+                <div className="LP_Links_Container">
+                    <div className="LP_Links_Header">
+                        <a href="#" className="Menu_Link">
+                            <span className="material-icons LP_Links_Icon">flag_circle</span>Início
+                        </a>
+                    </div>
+                    <div className="LP_Links_Horizontal_Organizer">
+                        <div className="LP_Links_Line_Container">
+                            <div className="LP_Links_Line"></div>
+                        </div>
+                        <div className="LP_Links">
+                            <a href="#" className="Menu_Link">
+                                Sobre
+                            </a>
+                            <div className="Product_Links_Container">
+                                <div className="Product_Links_Header">
+                                    <a href="#" className="Menu_Link">
+                                        Produtos
+                                    </a>
+                                </div>
+                                <div className="LP_Links_Horizontal_Organizer">
+                                    <div className="LP_Links_Line_Container">
+                                        <div className="LP_Links_Line"></div>
+                                    </div>
+                                    <div className="Product_Links">
+                                        <a href="#" className="Menu_Link">
+                                            Produto 1
+                                        </a>
+                                        <a href="#" className="Menu_Link">
+                                            Produto 2
+                                        </a>
+                                        <a href="#" className="Menu_Link">
+                                            Produto 3
+                                        </a>
+                                        <a href="#" className="Menu_Link">
+                                            Produto 4
+                                        </a>
+                                        <a href="#" className="Menu_Link">
+                                            Produto 5
+                                        </a>
+                                        <a href="#" className="Menu_Link">
+                                            Produto 6
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <a href="#" className="Menu_Link">
+                                Contato
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="Secondary_Links">
+                    <a href="#Privacidade" className="Menu_Link_Secondary">
+                        <span className="material-icons">privacy_tip</span>Privacidade
+                    </a>
+                    <a href="#Termos" className="Menu_Link_Secondary">
+                        <span className="material-icons">assignment</span>
+                        Termos
+                    </a>
+                    <a href="#Mapa do Site" className="Menu_Link_Secondary">
+                        <span className="material-icons">map</span>Mapa do Site
+                    </a>
+                </div>
+
+                <div className="Configurations">
+                    <div className="Configurations_Header">
+                        <span className="material-icons LP_Links_Icon">settings</span>
+                        <p className="Menu_Link">Configurações</p>
+                    </div>
+                    <div className="LP_Links_Horizontal_Organizer">
+                        <div className="LP_Links_Line_Container">
+                            <div className="LP_Links_Line"></div>
+                        </div>
+                        <div className="Configurations_Links">
+                            <a href="#Idioma" className="Menu_Link Config_Link">
+                                Idioma: <span className="Config_Option active">Português Brasileiro</span>
+                                <span className="Config_Option">English</span>
+                            </a>
+                            <a href="#Moeda" className="Menu_Link">
+                                Moeda: <span className="Config_Option active">Real (R$)</span> <span className="Config_Option">Dollar (USD)</span>
+                            </a>
+                            <a href="#Cores" className="Menu_Link">
+                                Esquema de Cores: <span className="Config_Option active">Escuro</span>
+                                <span className="Config_Option">Claro</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </m.div>
     );
 }
