b9d5765ec892a71af0e1ec182e1f862163a0cbd9
JOAO TARRAN <joaotarran@gmail.com>
2023-07-11 17:46:09 -0300

    very simple search alg

--- 02_Projetos/02_TropicalCacau/tc_website_final/src/components/02B_Search_Bar.tsx
+++ 02_Projetos/02_TropicalCacau/tc_website_final/src/components/02B_Search_Bar.tsx
@@ -1,3 +1,9 @@
+// Vanilla React Imports
+import { useState } from "react";
+
+// React Redux Imports
+import { useSelector } from "react-redux";
+
 //Framer Motion Imports
 import { motion as m } from "framer-motion";
 
@@ -7,9 +13,24 @@ import { toggleSearch } from "../context/main_context";
 
 export default function SearchBar() {
     const dispatch = useDispatch();
+    const [searchQuery, setSearchQuery] = useState("");
+    const availableChocolates = useSelector((state: any) => state.availableChocolates);
+    const [filteredChocolates, setFilteredChocolates] = useState([]); // Store filtered chocolates in local state
 
     const toggleSearchButton = () => {
         dispatch(toggleSearch());
+    };
+
+    const handleSearch = (event) => {
+        const query = event.target.value;
+        setSearchQuery(query);
+        const filtered = searchChocolates(query);
+        setFilteredChocolates(filtered); // Update the filtered chocolates in local state
+    };
+
+    const searchChocolates = (query) => {
+        const filtered = availableChocolates.filter((chocolate: any) => chocolate.name.toLowerCase().includes(query.toLowerCase()));
+        return filtered;
     };
 
     return (
@@ -22,7 +43,7 @@ export default function SearchBar() {
                 </button>
             </div>
             <div className="SearchBar_Input_Container">
-                <input type="text" className="SearchBar_Input" placeholder="Pesquisar..." />
+                <input type="text" className="SearchBar_Input" placeholder="Pesquisar..." value={searchQuery} onChange={handleSearch} />
                 <button className="SearchBar_Button">
                     <span className="material-icons">search</span>
                 </button>
@@ -31,7 +52,16 @@ export default function SearchBar() {
             <div className="SearchBar_Result_Container">
                 <p className="SearchBar_Result_Title">Resultados:</p>
                 <div className="SearchBar_Result_List">
-                    <p>Nenhuma pesquisa foi feita.</p>
+                    {searchQuery.length > 0 ? (
+                        // Render the search results based on the filteredChocolates array
+                        filteredChocolates.length > 0 ? (
+                            filteredChocolates.map((chocolate: any) => <p key={chocolate.name}>{chocolate.name}</p>)
+                        ) : (
+                            <p>Nenhum resultado encontrado.</p>
+                        )
+                    ) : (
+                        <p>Nenhuma pesquisa foi feita.</p>
+                    )}
                 </div>
             </div>
         </m.div>
